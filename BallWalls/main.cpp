ca8ca94c77e27ce11ae85ce4e1498b2fed32556b
Unknown <kevin.taufer@web.de>
2018-03-19 16:24:05 +0100

    Pin Belegung Neu

--- BallWalls/main.cpp
+++ BallWalls/main.cpp
@@ -2,7 +2,7 @@
 * BallWall.cpp
 *
 * Created: 19.09.2017 11:35:06
-* Authors : ktaufer,mzudrell,bbrüstle
+* Authors : Kevin Taufer
 */
 
 #include <avr/io.h>
@@ -10,22 +10,25 @@
 
 void execute_steps(void);
 
-#define icrValue 23999
+#define icrValue 64000
 
 volatile bool dirUp;
 volatile bool enabled;
-unsigned char spiValues[4];
+unsigned char spiValues[7];
 
 int main(void)
 {
 	/* set outputs */
 	DDRB |= (1 << PB5);					// PWM
-	DDRA |= (1 << PA1) ;				// INT0 für Raspberry
+	DDRB |= (1 << PB3) ;				// INT0 für Raspberry
 	
 	DDRF = 0xFF;
 	DDRC = 0xFF;
 	DDRK = 0xFF;
 	DDRH = 0xFF;
+	DDRL = 0xFF;
+	DDRA = 0xFF;
+	DDRD = 0xFF;
 
 	/* activate SPI */
 	SPCR |= (1<<SPE);	
@@ -36,14 +39,14 @@ int main(void)
 	TCCR1A |= (1 << COM1A1);					// PWM auf Ausgang OC1A aktivieren (PB5)
 	
 	ICR1 = (uint16_t)icrValue;
-	OCR1A = 15999;
-	OCR1B = 19999;
-	TIMSK1 |= (1 << OCIE1B) | (1 << ICIE1);				//Interrupt
+	OCR1A = icrValue/2;
+	
+	TIMSK1 |= (1 << OCIE1A) | (1 << ICIE1);				//Interrupt
 	
 	/* activate global interrupts */
 	sei();
-
-	TCCR1B |= (1 << CS10);
+	TCCR1B &= ~((1 << CS12) | (1 << CS11) | (1 << CS10));
+	TCCR1B |= (1 << CS10); // Prescaler 1
 	
 	while (1)
 	{
@@ -59,28 +62,48 @@ uint8_t SPI_SlaveReceive(void){
 }
 
 /* Interrupt routine (reaching OCR value) */
-ISR(TIMER1_COMPB_vect) {
+ISR(TIMER1_COMPA_vect) {
 	execute_steps();
 }
 
 /* Interrupt  routine (reaching ICR value)*/
 ISR(TIMER1_CAPT_vect) {
 	/* callback ready to get values to raspberry pi */
-	PORTA |= (1 << PA1);
-	for(uint8_t i = 0; i <= 3; i++) {
+	PORTA |= (1 << PB3);
+	for(uint8_t i = 0; i <= 7; i++) {
 		spiValues[i] = SPI_SlaveReceive();
 	}
-	PORTA &= ~(1 << PA1);
+	PORTB &= ~(1 << PB3);
 }
 
 /* writes values of SPI to ports */
-void execute_steps(void){
+		void execute_steps(void){
 
-	PORTF = spiValues[0];
+			PORTF = spiValues[0];
 
-	PORTK = spiValues[1];
+			PORTK = spiValues[1];
 
-	PORTH = spiValues[2];
-
-	PORTC = spiValues[3];	
+			PORTL = spiValues[2];
+			
+			PORTA = spiValues[3];
+			
+			PORTC = spiValues[4];
+			
+			unsigned char wert;
+			
+			// Set PORTD			
+			PORTD = (spiValues[5] & 0b00001111);
+			PORTD = PORTD | ((0b00010000 & spiValues[5]) << 3);
+			
+			// Set PORTG		
+			PORTG = (spiValues[5] & 0b11100000) >> 5; 					
+			
+			// Set PORTH
+			PORTH = (spiValues[6] & 0b00000011) | ((spiValues[6] & 0b00111100) << 1);
+			
+			// Set PORTJ
+			PORTJ = ((spiValues[6] & 0b11000000) >> 6);
+			
+			// Set PORTB
+			PORTB = (spiValues[7] << 4) | (PORTB & 00001111);
 };
